07ab91516c308e13191ff6480a9090bb7ac57caa
shubhankar <shubhankar@to-on.com>
2019-06-19 17:25:41 +0900

    Capitalize solution 2

--- DSandAlgo/exercises/capitalize/index.js
+++ DSandAlgo/exercises/capitalize/index.js
@@ -7,15 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-capitalize('this is my! string')
+capitalize3('this is my! string')
 
-function capitalize(str) {
+function capitalize1(str) {
   
   let capitalizedArray = [];
   let temp = str.split(' ');
-  console.log(temp);
 
- for(const word of temp) {
+ for(let word of temp) {
     let firstLetter = word.slice(0,1);
     let remainingWord = word.slice(1, word.length);
     firstLetter = firstLetter.toUpperCase();
@@ -27,4 +26,45 @@ function capitalize(str) {
   console.log(capitalizedString)
 }
 
-module.exports = capitalize;
+// Divide the words in array
+// Traverse array and capitalize each starting letter of array
+function capitalize2 (string) {
+  const words = [];
+
+  for (let word of string.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  words.join(' ');
+  console.log("RESULT");
+  console.log(words);
+}
+
+// Detect ' ' (spaces) in string and capitalize the letter after the space and add it to array.
+function capitalize3(string) {
+
+  let capitalizeNext = true;
+  let array = string.split('');
+
+  let temp = [];
+  for(let i = 0; i< array.length; i++){
+    if(capitalizeNext){
+      temp.push(array[i].toUpperCase());
+    } else {
+      temp.push(array[i]);
+    }
+
+    if(array[i] === ' '){
+      capitalizeNext = true;
+    } else {
+      capitalizeNext = false;
+    }
+
+  }
+
+  console.log("RESULT1");
+  console.log(temp.join(''));
+
+}
+
+module.exports = capitalize3;
