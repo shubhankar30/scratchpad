7017a8eb622320799dcb6c45cd01b8e073ec4196
Shubhankar Ranade <shubhankarranade30@gmail.com>
2019-08-04 22:21:53 +0900

    Add bubble sort

--- DSandAlgo/exercises/sorting/index.js
+++ DSandAlgo/exercises/sorting/index.js
@@ -2,7 +2,16 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for(let i = 0; i<arr.length; i++) {
+        for(let j = 0; j< (arr.length -i -1); j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
@@ -17,4 +26,8 @@ function merge(left, right) {
 
 }
 
+let arr = [9,8,7,6,4,3,2];
+
+let yolo = bubbleSort(arr)
+console.log(yolo);
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
